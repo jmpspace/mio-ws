24bfbc358897d91b1d4cb8848e830f20a6e47507
John P Mayer <john.p.mayer.jr@gmail.com>
2015-03-10 23:16:59 -0400

    this seems to not work!

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,4 +1,59 @@
+// unstable libraries
+#![feature(io,net)]
+
+extern crate "websocket-protocol" as ws;
+
+use std::io::{Error};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use ws::protocol::{WsError, WebSocketStream};
+
+fn handle_client(stream: TcpStream) -> Result<(), WsError> {
+
+    println!("Handling client");
+    
+    let mut ws = try!(WebSocketStream::new(stream));
+    
+    println!("Opened ws");
+
+    try!(ws.send(format!("Test message").as_bytes()));
+    
+    loop {
+        let msg = try!(ws.recv());
+        try!(ws.send(&msg[..]));
+    }
+    
+}
+
+fn run() -> Result<(), Error> {
+
+    let listener = try!(TcpListener::bind("0.0.0.0:9000"));
+
+    for stream in listener.incoming() {
+        
+        match stream {
+            Err(e) => { 
+                println!("Accept error: {:?}", e); 
+            }
+            Ok(stream) => {
+              thread::spawn(move|| {
+                match handle_client(stream) {
+                    Ok(()) => println!("Client quit without error"),
+                    Err(e) => println!("Client exited with error: {:?}", e)
+                };
+              });
+            }
+        }
+    }
+    
+    return Ok(());
+}
+
 fn main() {
-    // TODO mio goes here
-    println!("Hello, world!");
+  match run() {
+    Ok(_) => return,
+    Err(e) => {
+      println!("Got error {:?}", e);
+    }
+  }
 }
